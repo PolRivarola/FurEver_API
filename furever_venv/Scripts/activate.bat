43d73886434d6f69965fc19d8674edc313fcd398
Marco Mignacco <marcomignacco03@gmail.com>
2023-10-03 20:16:25 -0300

    venv setup

--- furever_venv/Scripts/activate.bat
+++ furever_venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\rivar\OneDrive\Escritorio\Siglo\Seminario 1\FurEver_api\furever_venv
+set VIRTUAL_ENV=C:\Users\marco\OneDrive\Desktop\FurEver_API\furever_venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,6 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
+set VIRTUAL_ENV_PROMPT=(furever_venv) 
 
 :END
 if defined _OLD_CODEPAGE (
